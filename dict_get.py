d68f43a042d056783e85dd0f93c6055f4db9cd92
hifuer <50453739+hifuer@users.noreply.github.com>
2019-12-01 22:11:11 +0800

    Update dict_get.py

--- dict_get.py
+++ dict_get.py
@@ -3,8 +3,13 @@ name_for_userid={
     590:"Bob",
     951:"Dilbert",
     }
+'''
+用字典get()方法设置字典默认值 （）
+'''
 def greeting(userid):
-    return "Hi %s" % name_for_userid.get(userid,"there")
+    return "Hi %s" % name_for_userid.get(userid,"there") 
 
-print(greeting(382))
-print(greeting(33333))
+print(greeting(382)) 
+#字典内 输出 Hi Ailce
+print(greeting(33333)) 
+#字典外 输出 Hi there
